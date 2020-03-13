254981a390724a3c456041e907cf6ec81cc196e9
crysis <crysiswarhead020304050@gmail.com>
2020-03-13 14:46:32 +0200

    Add password input pattern and message

--- src/validationSchema/index.js
+++ src/validationSchema/index.js
@@ -1,17 +1,19 @@
 import * as yup from 'yup';
 
 const NAME_PATTERN = /^[A-Z][a-z]{0,63}$/;
+const PASSWORD_PATTERN = /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?\d)[A-Za-z0-9_@#%!?\-^]{8,60}$/;
 const namePatternMessage = name => `${name} name must starts with capital letter, contains only alphabet letters and be not longer than 63 characters`;
+const passwordPatternMessage = 'Password should be 8 to 60 characters long, must contain uppercase and lowercase letters and numbers';
 
 export const signUpSchema = yup.object().shape({
     firstName: yup.string().required().matches(NAME_PATTERN, namePatternMessage('First')).label('First name'),
     lastName: yup.string().required().matches(NAME_PATTERN, namePatternMessage('Last')).label('Last name'),
     email: yup.string().email().required().label('Email'),
-    password: yup.string().min(8).required().label('Password'),
-    confirmPassword: yup.string().min(8).required().oneOf([yup.ref('password')], 'Both password need to be the same').label('Confirm password'),
+    password: yup.string().min(8).required().matches(PASSWORD_PATTERN, passwordPatternMessage).label('Password'),
+    confirmPassword: yup.string().required().oneOf([yup.ref('password')], 'Both password need to be the same').label('Confirm password'),
 });
 
 export const signInSchema = yup.object().shape({
                                                    email: yup.string().email().required().label('Email'),
-                                                   password: yup.string().min(8).required().label('Password'),
+                                                   password: yup.string().min(8).required().matches(PASSWORD_PATTERN, passwordPatternMessage).label('Password'),
                                                });
